d748fb704d13b09a024fcf4ccff55a8e95b4fdd5
elizabeteprounicorn <elizabete.bleidele@je.pvg.edu.lv>
2026-04-27 23:30:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function openPage(page) {
     window.location.href = page;
 }
+
+
+function celebrate() {
+    for (let i = 0; i < 50; i++) {
+        let confetti = document.createElement("div");
+        confetti.innerHTML = "🎉";
+        confetti.style.position = "fixed";
+        confetti.style.left = Math.random() * 100 + "vw";
+        confetti.style.top = "-20px";
+        confetti.style.fontSize = "24px";
+        document.body.appendChild(confetti);
+
+        let fall = setInterval(() => {
+            confetti.style.top = confetti.offsetTop + 5 + "px";
+            if (confetti.offsetTop > window.innerHeight) {
+                confetti.remove();
+                clearInterval(fall);
+            }
+        }, 30);
+    }
+}
